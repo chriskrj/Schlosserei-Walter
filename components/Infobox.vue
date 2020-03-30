945536c702fab811ef0fe9907771959c802dfdd9
Christopher Krawietze <chriskrawietz@gmail.com>
2020-03-30 15:33:30 +0200

    added infobox

--- components/Infobox.vue
+++ components/Infobox.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="">
-
+  <div class="Infobox-container">
+    <div class="Infobox">
+      <h3 class="Base-h3">Unsere Öffnungfszeiten</h3>
+      <div class="Infobox-content">
+        <p><strong>Montags - Donnerstags</strong></p>
+        </div>
+    </div>
   </div>
 </template>
