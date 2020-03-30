c8d0cacdac351337f119549ab0e1098121ce2624
Christopher Krawietze <chriskrawietz@gmail.com>
2020-03-30 17:26:37 +0200

    added some lorem text to layout

--- components/Infobox.vue
+++ components/Infobox.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="Infobox-container">
+  <div class="Infobox-container Base-theme--dark">
     <div class="Infobox">
-      <h3 class="Base-h3">Unsere Öffnungfszeiten</h3>
-      <div class="Infobox-content">
-        <p><strong>Montags - Donnerstags</strong></p>
-        <p>Werkstatt: 07:00 Uhr - 15:45 Uhr<br/>
+      <h3 class="Base-h3 u-textAlign--center">Unsere Öffnungfszeiten</h3>
+      <div class="Infobox-content u-textAlign--center">
+        <p class="Base-p"><strong>Montags - Donnerstags</strong></p>
+        <p class="Base-p">Werkstatt: 07:00 Uhr - 15:45 Uhr<br/>
         Büro: 08:00 Uhr - 15:45 Uhr</p>
-        <p><strong>Freitags</strong></p>
-        <p>Werkstatt: 07:00 Uhr - 13:15 Uhr<br/>
+        <p class="Base-p"><strong>Freitags</strong></p>
+        <p class="Base-p">Werkstatt: 07:00 Uhr - 13:15 Uhr<br/>
         Büro: 08:00 Uhr - 13:15 Uhr</p>
         </div>
     </div>
@@ -16,11 +16,11 @@
 
 <style lang="scss" scoped>
   .Infobox-container {
-    background-color: --var(--main-bg-color);
+    background-color: var(--main-bg-color);
     border-radius: 8px;
   }
 
   .Infobox {
-    padding: calc($susy-gutter-grid * 2vw);
+    padding: calc(16px + 2vw);
   }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,22 @@
 <template>
-  <div class="u-container">
+  <div class="u-container u-marginBottom--r2 u-marginTop--r2">
     <div class="u-limitWidth-12 u-align-center">
       <div class="GridCol2MainAside-row u-row">
         <div class="GridCol2MainAside-col-main u-col">
-          <h1>Herzlich Willkommen bei der Firma Walter Boike Schlosserei und Zerspanungstechnik</h1>
+          <h1 class="Base-h1">Herzlich Willkommen bei der Firma Walter Boike Schlosserei und Zerspanungstechnik</h1>
+          <div class="MainContent">
+              <p class="Base-p">
+                Die Firma Schlosserei Boike...
+              </p>
+              <p class="Base-p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, eos optio! Vero, optio magni sapiente illo ea aut, dignissimos incidunt labore est ab sed quam, ipsa voluptatibus officia soluta voluptas.</p>
+          </div>
         </div>
         <div class="GridCol2MainAside-col-aside u-col">
           <Infobox/>
         </div>
       </div>
     </div>
+
   </div>
 </template>
 
