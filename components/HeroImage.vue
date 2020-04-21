95988b0a055bfcf51cb18322573f10b860e22645
Christopher Krawietze <chriskrawietz@gmail.com>
2020-04-21 21:49:54 +0200

    changed heroiamge

--- components/HeroImage.vue
+++ components/HeroImage.vue
@@ -1,15 +1,3 @@
 <template>
     <div class="HeroImage"></div>
 </template>
-
-<style lang="scss" scoped>
-  .HeroImage {
-    position: relative;
-    width: 100%;
-    height: 500px;
-    background-image: url("../assets/Images/heroimage.jpg");
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: center center;
-  }
-</style>
