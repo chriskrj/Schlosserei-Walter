ce725dd1dd114c2ff4e25d62eb71d5096f76c68f
Christopher Krawietz <krawietz@new-communication.de>
2020-06-09 10:21:55 +0200

    added meta and link tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,13 +8,24 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { name: 'apple-mobile-web-app-title', content: 'Schlosserei Boike' },
+      { name: 'application-name', content: 'Schlosserei Boike' },
+      { name: 'msapplication-TileColor', content: '#003996' },
+      { name: 'msapplication-TileImage', content: '/mstile-144x144.png' },
+      { name: 'theme-color', content: '#ffffff' },
+      { hid: 'description', name: 'description', content: 'Schlosserei Boike' }
     ],
     link: [
+      { rel: 'apple-touch-icon',sizes: '180x180', type: 'image/png', href: '/apple-touch-icon.png' },
+      { rel: 'icon',sizes: '32x32',type: 'image/png', href: '/favicon-32x32.png' },
+      { rel: 'icon',sizes: '16x16',type: 'image/png', href: '/favicon-16x16.png' },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color:'#003996' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto&display=swap' }
     ]
   },
+  
   /*
   ** Customize the progress bar color
   */
